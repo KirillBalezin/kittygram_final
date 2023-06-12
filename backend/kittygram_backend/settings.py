745056ccf3a28655aa892bb9c618c576e1177307
Кирилл Балезин <Kirill.Balezin@yandex.ru>
2023-06-12 14:41:27 +0300

    3.0

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,14 +1,19 @@
 # flake8: noqa
 import os
 from pathlib import Path
+from distutils.util import strtobool
+
+from dotenv import load_dotenv 
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
+DEBUG = bool(strtobool(os.getenv('DEBUG', 'False')))
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  os.environ.get(
+    'ALLOWED_HOSTS', default='localhost').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
